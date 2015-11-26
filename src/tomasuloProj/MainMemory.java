07f10ccf5bd726f092de7842a2f9caf98503b5cb
Doola <ahmed.adel.abodeif@gmail.com>
2015-11-26 19:03:13 +0200

    fixing direct mapped

--- src/tomasuloProj/MainMemory.java
+++ src/tomasuloProj/MainMemory.java
@@ -13,38 +13,54 @@ public class MainMemory {
 	}
 	
 	// Must check that address of data is not > 64KB
-	public static void Insert(String address , String data, int blockSize)
-	{
-		// extracting 16 bit words from given block
-		String[] dataCopied = new String[blockSize];
-		for(int i=0; i<blockSize; i++)
-		{
-			dataCopied[i] = data.substring(i*16, i*16+16);
-		}
-		
-		// placing extracted words in corresponding addresses
+	public static void Insert(String address , String[] data, int blockSize)
+	{	
 		int add = Integer.parseInt(address,2);
-		for(int i=0; i<blockSize; i++)
-		{
-			RAM.put(Integer.toBinaryString(add),dataCopied[i]);
-			add++;
+		for (int i = 0; i < data.length; i++) {
+			RAM.put(Integer.toBinaryString(i+add), data[i]);
 		}
 	}
 	
-	public static String Read(int wordAddress, int blockSize)
+	// This will take address of a word and word itself to add
+	// We need this method because each cache has a different block size
+	public static void WriteWord(String address, String data)
 	{
+		RAM.put(address, data);
+	}
+	
+	// In the cache, We find out the starting address of the block that 
+	// we are trying to fetch so we copy starting from the given address
+	public static String[] Read(int wordAddress, int blockSize)
+	{
+		
+		
+		// find where the start of the block that contains this word address
+		
+		
+		
+		///////////////////////////////////////////////////////
+		String[] block = new String[blockSize];
+		for (int i = 0; i < block.length; i++) {
+			block[i] = RAM.get(Integer.toBinaryString(wordAddress + i));
+		}
+		return block;
+		
+		
+		
+		
+		//////////////////////////////////////////////////////
 		// must return block equivilant to block size of cache
 		// fetch addresses in order till block is full
-		
-		String block = "";
-		for(int i=0; i<blockSize; i++)
-		{
-			String address = Integer.toBinaryString(wordAddress);
-			block += RAM.get(address);
-			wordAddress++;
-		}
-		
-		return block;
+//		
+//		String block = "";
+//		for(int i=0; i<blockSize; i++)
+//		{
+//			String address = Integer.toBinaryString(wordAddress);
+//			block += RAM.get(address);
+//			wordAddress++;
+//		}
+//		
+//		return block;
 	
 	}
 	
--- src/tomasuloProj/TheBigCache.java
+++ src/tomasuloProj/TheBigCache.java
@@ -8,6 +8,7 @@ import tomasuloProj.MainMemory;
 public abstract class TheBigCache implements Cache{
 	int Size, BlockSize, assosciativity, lengthIndex, lengthOffset, lengthTag;
 	boolean WriteBack, WriteThrough;
+	
 	static LinkedList<TheBigCache> hier;
 	static int currentCachePosition = 0;
 	
@@ -25,13 +26,17 @@ public abstract class TheBigCache implements Cache{
 			if(hier.get(i).Read(wordAddress)!= null)
 				return hier.get(i).Read(wordAddress);
 		}
-		
+		// this address is in main memory so we need to fetch it from there
+		// the block size is different for each cache so we need to pass block size
+		// and call this recursively till we are at the top level.
 		addToCache(wordAddress);
 		
 		return MainMemory.ReadTemp(Integer.toBinaryString(wordAddress));
 	}
 
 	@Override
+	// this should take only the byte of data that needs to be changed
+	// because the size of blocks is different from level to level.
 	public void Write(int wordAddress, String data) {
 		// TODO Auto-generated method stub
 		
